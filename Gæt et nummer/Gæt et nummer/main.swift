a8dac4cf0a44f768ec7d844618173c2dfc5e2382
Emil Neirup Jensen <emilnjensen@gmail.com>
2018-05-08 13:08:23 +0200

    Code build

--- Gæt et nummer/Gæt et nummer/main.swift
+++ Gæt et nummer/Gæt et nummer/main.swift
@@ -10,4 +10,37 @@ import Foundation
 
 var forsøg = 7
 
-var randomNumber = arc4random_Uniform(100)
+let randomNumber = arc4random_uniform(101)
+
+var vundet: Bool = false
+
+print("Gæt et tal mellem 0 og 100 :-)")
+
+
+while forsøg > 0 {
+    
+    let temp:String = readLine() ?? ""
+    let int:Int = Int(temp) ?? 0
+
+    if int == randomNumber {
+        print("Najs - Godt gættet")
+        vundet = true
+        break
+    }
+    else if int > randomNumber {
+        print("Det er mindre end \(int)")
+        }
+
+    else {
+        print("Det er større end \(int)")
+        }
+    forsøg -= 1
+    print("Du har \(forsøg) forsøg tilbage")
+}
+
+if vundet{
+    print("Du har vundet")
+} else {
+    print("Du har tabt")
+}
+
