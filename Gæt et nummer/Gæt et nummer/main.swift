bcbffb33c98c2b93f065cca91dc3d5faeb70d5e3
Nikolai Povlsen <nikolaipovlsen@yahoo.dk>
2018-05-09 08:10:14 +0200

    Rotated if statement

--- Gæt et nummer/Gæt et nummer/main.swift
+++ Gæt et nummer/Gæt et nummer/main.swift
@@ -13,4 +13,4 @@ for i in stride(from: 7, to: 0, by: -1) { let int:Int = Int(readLine() ?? "") ??
 print("Gæt et tal mellem 0 og 100 - Begge tal inklusiv:-)")
 for i in stride(from: 7, to: 0, by: -1) { let int:Int = Int(readLine() ?? "") ?? 0
     if int == randomNumber { print("Du har vundet") } else if int > randomNumber { print("Det er mindre end \(int), du har \(i-1) forsøg tilbage") } else { print("Det er større end \(int), du har \(i-1) forsøg tilbage") }
-    if (i-1 <= 0) { print("Du har tabt") } else if int == randomNumber{ break} }
+      if int == randomNumber{ break} } else if (i-1 <= 0) { print("Du har tabt") }
